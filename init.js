1280bb19ef76f132dd1d80055aa512b9abe49810
Justin Harrelson <jmharrels@gmail.com>
2021-03-28 15:13:53 -0400

    Initial checkin of package.json, init script, and base HTML

---
+++ init.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const windowStateKeeper = require('electron-window-state');
+
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1024,
+    defaultHeight: 600
+  });
+  // Create the browser window.
+  let win = new BrowserWindow({
+    show: false,
+    backgroundColor: "#888",
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    frame: false,
+    minWidth: 1024,
+    minHeight: 600,
+    webPreferences: {
+      enableRemoteModule: true,
+      nodeIntegration: true,
+      spellcheck: true
+    }
+  })
+
+  mainWindowState.manage(win);
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('scummy.html')
+}
+app.whenReady().then(createWindow)
