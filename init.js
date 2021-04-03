26295b58eb3b201b90ec34ecfa5005d1539b2b86
Justin Harrelson <jmharrels@gmail.com>
2021-04-02 20:46:21 -0400

    Updated Electron to latest version. Fixed up init script to handle the new Electron.

--- init.js
+++ init.js
@@ -22,6 +22,7 @@ function createWindow () {
     minHeight: 600,
     webPreferences: {
       enableRemoteModule: true,
+      contextIsolation: false,
       nodeIntegration: true,
       spellcheck: false
     }
